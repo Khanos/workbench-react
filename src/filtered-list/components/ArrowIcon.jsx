169eb613a8380a8d85a11d4dfb695622c9de24f6
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-04-04 11:13:06 -0400

    checking useCallback pitfalls

--- src/filtered-list/components/ArrowIcon.jsx
+++ src/filtered-list/components/ArrowIcon.jsx
@@ -1,11 +1,13 @@
+import { useCallback } from "react";
+
 const ArrowIcon = ({identifier, filter, setFilter}) => {
-  const sortHandler = (key) => {
+  const sortHandler = useCallback((key) => {
     const newFilter = { col: key, dir: 'asc' };
     if(filter.col === key && filter.dir === 'asc'){
       newFilter.dir = 'desc';
     }
     setFilter(newFilter);
-  };
+  }, [filter.col, filter.dir, setFilter]);
   if(filter.col !== identifier) return <div onClick={() => sortHandler(identifier)} className="btn-filter">&#8722;</div>;
   return filter.dir === 'desc' ? 
     <div onClick={() => sortHandler(identifier)} className="btn-filter">&#8593;</div> : 
